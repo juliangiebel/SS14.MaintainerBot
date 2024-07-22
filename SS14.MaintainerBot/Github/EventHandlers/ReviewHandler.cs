4eaa63bfce0748dd99b1b6b2d810f34e517d8c96
juliangiebel <juliangiebel@live.de>
2024-07-22 09:30:25 +0200

    Work on pull request event handling

--- SS14.MaintainerBot/Github/EventHandlers/ReviewHandler.cs
+++ SS14.MaintainerBot/Github/EventHandlers/ReviewHandler.cs
@@ -1,5 +1,6 @@
 ﻿using FastEndpoints;
 using JetBrains.Annotations;
+using Octokit;
 using SS14.MaintainerBot.Github.Events;
 
 namespace SS14.MaintainerBot.Github.EventHandlers;
@@ -7,7 +8,34 @@ namespace SS14.MaintainerBot.Github.EventHandlers;
 [UsedImplicitly]
 public class ReviewHandler : IEventHandler<ReviewEvent>
 {
-    public Task HandleAsync(ReviewEvent eventModel, CancellationToken ct)
+    private const string DismissedAction = "dismissed";
+
+    public async Task HandleAsync(ReviewEvent eventModel, CancellationToken ct)
+    {
+        if (eventModel.Action == DismissedAction)
+            return;
+        
+
+        // ReSharper disable once SwitchStatementMissingSomeEnumCasesNoDefault
+        switch (eventModel.Review.State.Value)
+        {
+            case PullRequestReviewState.Approved: await OnPrApproved(eventModel.Review, ct); break;
+            case PullRequestReviewState.ChangesRequested: await OnPrChangesRequested(eventModel.Review, ct); break;
+        }
+    }
+
+    private async Task OnPrApproved(PullRequestReview eventModelReview, CancellationToken ct)
+    {
+        
+        // TODO: Check Pull request requirements
+        // TODO: If configured to create messages and threads on approval instead of pr opening. Post them
+        // TODO: Update PR in DB
+        // TODO: Post or update PR comment
+        // TODO: Start PR merge timer
+        throw new NotImplementedException();
+    }
+    
+    private async Task OnPrChangesRequested(PullRequestReview eventModelReview, CancellationToken ct)
     {
         throw new NotImplementedException();
     }
---
+++ SS14.MaintainerBot/Github/Services/PrVerificationService.cs
@@ -0,0 +1,18 @@
+﻿using Octokit;
+
+namespace SS14.MaintainerBot.Github.Helpers;
+
+public class PrVerificationService
+{
+    private readonly GithubBotConfiguration _configuration;
+
+    public PrVerificationService(IConfiguration configuration)
+    {
+        configuration.Bind(GithubBotConfiguration.Name, _configuration);
+    }
+
+    public bool CheckGeneralRequirements(PullRequest pullRequest)
+    {
+        
+    }
+}
