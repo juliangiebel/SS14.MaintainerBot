67f669fffb548fd22d9701ce8327d5b1078a9f33
juliangiebel <juliangiebel@live.de>
2024-08-09 16:59:36 +0200

    Work on forum posting and role restrictions

--- SS14.MaintainerBot/Discord/Commands/CreateForumPost.cs
+++
@@ -1,8 +0,0 @@
-﻿using FastEndpoints;
-
-namespace SS14.MaintainerBot.Discord.Commands;
-
-public record CreateForumPost (
-    ulong GuildId,
-    string Title
-    ) : ICommand<Guid?>;
---
+++ SS14.MaintainerBot/Discord/Commands/CreateOrUpdateForumPost.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+using FastEndpoints;
+using SS14.MaintainerBot.Discord.Entities;
+
+namespace SS14.MaintainerBot.Discord.Commands;
+
+public record CreateForumPost (
+    Guid MergeProcessId,
+    ulong GuildId,
+    string Title,
+    string Content,
+    List<ButtonDefinition>? Buttons = null
+    ) : ICommand<DiscordMessage?>;
+    
+public record ButtonDefinition(string Title, string Id, ButtonStyle Style);
